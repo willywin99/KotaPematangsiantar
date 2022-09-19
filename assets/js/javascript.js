3e9c5603c476e0919a2c7c58e9a18c9acb830658
willywin99 <willywinsianturi@gmail.com>
2022-09-19 15:14:52 +0700

    feat: add logo with favicon

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -16,5 +16,20 @@ $('.page-scroll').on('click', function(e) {
   }, 1250, 'easeInOutExpo');
 
   e.preventDefault();
+});
 
+// event pada saat link di klik
+$('.logo-container').on('click', function(e) {
+
+  // ambil isi href
+  var tujuan = $(this).attr('href');
+  // tangkap elemen yang bersangkutan
+  var elemenTujuan = $(tujuan);
+
+  // pindahkan scroll
+  $('html,body').animate({
+    scrollTop: elemenTujuan.offset().top
+  }, 1250, 'easeInOutExpo');
+
+  e.preventDefault();
 });
