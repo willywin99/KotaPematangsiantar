f40a1429257d7769530b6a5a9ff77384467e93d1
willywin99 <willywinsianturi@gmail.com>
2024-06-17 22:05:26 +0700

    fix: update trigger on screen size change

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,104 +1,79 @@
 var tglbtn = document.getElementById("nav-toggle");
-var navlst = document.getElementById("nav-list")
-tglbtn.addEventListener('click', () => {navlst.classList.toggle("active");});
+var navlst = document.getElementById("nav-list");
+tglbtn.addEventListener("click", () => {
+	navlst.classList.toggle("active");
+});
 
 // Create a media condition that targets viewports at least 600px wide
-const mediaQuery = window.matchMedia('(max-width: 600px)')
-// Check if the media query is true
-if (mediaQuery.matches) {
-  // event pada saat link di klik
-  $('.page-scroll').on('click', function(e) {
+const mediaQuery = window.matchMedia("(max-width: 600px)");
 
-    // ambil isi href
-    var tujuan = $(this).attr('href');
-    // tangkap elemen yang bersangkutan
-    var elemenTujuan = $(tujuan);
+function screenTest(e) {
+	// Check if the media query is true
+	if (e.matches) {
+		// event pada saat link di klik
+		$(".page-scroll").on("click", function (e) {
+			// ambil isi href
+			var tujuan = $(this).attr("href");
+			// tangkap elemen yang bersangkutan
+			var elemenTujuan = $(tujuan);
 
-    // pindahkan scroll
-    $('html,body').animate({
-      scrollTop: elemenTujuan.offset().top - 200
-    }, 1250, 'easeInOutExpo');
+			// pindahkan scroll
+			$("html,body").animate(
+				{
+					scrollTop: elemenTujuan.offset().top - 200,
+				},
+				2500,
+				"easeInOutExpo"
+			);
 
-    navlst.classList.toggle("active");
+			// navlst.classList.toggle("active");
 
-    e.preventDefault();
-  });
-} else {
-  // event pada saat link di klik
-  $('.page-scroll').on('click', function(e) {
-  
-    // ambil isi href
-    var tujuan = $(this).attr('href');
-    // tangkap elemen yang bersangkutan
-    var elemenTujuan = $(tujuan);
-  
-    // pindahkan scroll
-    $('html,body').animate({
-      scrollTop: elemenTujuan.offset().top
-    }, 1250, 'easeInOutExpo');
-  
-    navlst.classList.toggle("active");
-  
-    e.preventDefault();
-  });
+			e.preventDefault();
+		});
+	} else {
+		// event pada saat link di klik
+		$(".page-scroll").on("click", function (e) {
+			// ambil isi href
+			var tujuan = $(this).attr("href");
+			// tangkap elemen yang bersangkutan
+			var elemenTujuan = $(tujuan);
+
+			// pindahkan scroll
+			$("html,body").animate(
+				{
+					scrollTop: elemenTujuan.offset().top - 50,
+				},
+				2500,
+				"easeInOutExpo"
+			);
+
+			// navlst.classList.toggle("active");
+
+			e.preventDefault();
+		});
+	}
 }
 
-// Create a media condition that targets viewports at least 1000px wide
-const mediaQuery2 = window.matchMedia('(max-width: 1000px)')
-// Check if the media query is true
-if (mediaQuery2.matches) {
-  // event pada saat link di klik
-  $('.page-scroll').on('click', function(e) {
-
-    // ambil isi href
-    var tujuan = $(this).attr('href');
-    // tangkap elemen yang bersangkutan
-    var elemenTujuan = $(tujuan);
-
-    // pindahkan scroll
-    $('html,body').animate({
-      scrollTop: elemenTujuan.offset().top - 50
-    }, 1250, 'easeInOutExpo');
-
-    // navlst.classList.toggle("active");
-
-    e.preventDefault();
-  });
-} else {
-  // event pada saat link di klik
-  $('.page-scroll').on('click', function(e) {
-  
-    // ambil isi href
-    var tujuan = $(this).attr('href');
-    // tangkap elemen yang bersangkutan
-    var elemenTujuan = $(tujuan);
-  
-    // pindahkan scroll
-    $('html,body').animate({
-      scrollTop: elemenTujuan.offset().top
-    }, 1250, 'easeInOutExpo');
-  
-    // navlst.classList.toggle("active");
-  
-    e.preventDefault();
-  });
-}
-
+// Pada saat ada perubahan pada max-width dari versi desktop ke mobile atau sebaliknya maka event ini akan di-trigger
+mediaQuery.addEventListener("change", screenTest);
 
 // event pada saat link di klik
-$('.logo-container').on('click', function(e) {
+$(".logo-anchor").on("click", function (e) {
+	// ambil isi href
+	var tujuan = $(this).attr("href");
+	// tangkap elemen yang bersangkutan
+	var elemenTujuan = $(tujuan);
 
-  // ambil isi href
-  var tujuan = $(this).attr('href');
-  // tangkap elemen yang bersangkutan
-  var elemenTujuan = $(tujuan);
+	// pindahkan scroll
+	$("html,body").animate(
+		{
+			scrollTop: elemenTujuan.offset().top,
+		},
+		5000,
+		"easeInOutExpo"
+	);
 
-  // pindahkan scroll
-  $('html,body').animate({
-    scrollTop: elemenTujuan.offset().top
-  }, 1250, 'easeInOutExpo');
+	navlst.classList.toggle("active");
 
-  navlst.classList.toggle("active");
-
-  e.preventDefault();
+	e.preventDefault();
 });
