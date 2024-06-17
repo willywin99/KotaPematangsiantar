9def1f80643b7479eeebd2305ad8ff37908fb841
willywin99 <willywinsianturi@gmail.com>
2024-06-17 20:03:25 +0700

    feat: tambah wave pada footer dan sesuaikan animate scrolling sesuai max-width pada js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,21 +2,88 @@ var tglbtn = document.getElementById("nav-toggle");
 var navlst = document.getElementById("nav-list")
 tglbtn.addEventListener('click', () => {navlst.classList.toggle("active");});
 
-// event pada saat link di klik
-$('.page-scroll').on('click', function(e) {
+// Create a media condition that targets viewports at least 600px wide
+const mediaQuery = window.matchMedia('(max-width: 600px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // event pada saat link di klik
+  $('.page-scroll').on('click', function(e) {
 
-  // ambil isi href
-  var tujuan = $(this).attr('href');
-  // tangkap elemen yang bersangkutan
-  var elemenTujuan = $(tujuan);
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
 
-  // pindahkan scroll
-  $('html,body').animate({
-    scrollTop: elemenTujuan.offset().top - 50
-  }, 1250, 'easeInOutExpo');
+    // pindahkan scroll
+    $('html,body').animate({
+      scrollTop: elemenTujuan.offset().top - 200
+    }, 1250, 'easeInOutExpo');
 
-  e.preventDefault();
-});
+    navlst.classList.toggle("active");
+
+    e.preventDefault();
+  });
+} else {
+  // event pada saat link di klik
+  $('.page-scroll').on('click', function(e) {
+  
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
+  
+    // pindahkan scroll
+    $('html,body').animate({
+      scrollTop: elemenTujuan.offset().top
+    }, 1250, 'easeInOutExpo');
+  
+    navlst.classList.toggle("active");
+  
+    e.preventDefault();
+  });
+}
+
+// Create a media condition that targets viewports at least 1000px wide
+const mediaQuery2 = window.matchMedia('(max-width: 1000px)')
+// Check if the media query is true
+if (mediaQuery2.matches) {
+  // event pada saat link di klik
+  $('.page-scroll').on('click', function(e) {
+
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
+
+    // pindahkan scroll
+    $('html,body').animate({
+      scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo');
+
+    // navlst.classList.toggle("active");
+
+    e.preventDefault();
+  });
+} else {
+  // event pada saat link di klik
+  $('.page-scroll').on('click', function(e) {
+  
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
+  
+    // pindahkan scroll
+    $('html,body').animate({
+      scrollTop: elemenTujuan.offset().top
+    }, 1250, 'easeInOutExpo');
+  
+    // navlst.classList.toggle("active");
+  
+    e.preventDefault();
+  });
+}
+
 
 // event pada saat link di klik
 $('.logo-container').on('click', function(e) {
@@ -30,6 +97,8 @@ $('.logo-container').on('click', function(e) {
   $('html,body').animate({
     scrollTop: elemenTujuan.offset().top
   }, 1250, 'easeInOutExpo');
+
+  navlst.classList.toggle("active");
 
   e.preventDefault();
 });
